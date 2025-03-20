6487e650695e833c0ff70c11645c949e8a393506
alexdev <alexdev.workenv@gmail.com>
2025-03-20 18:13:14 -0400

    add towns of Habana, Artemisa, Mayabeque, Matanzas, Cienfuegos

--- src/index.ts
+++ src/index.ts
@@ -1,118 +1,98 @@
 import db from './db-manage';
 
-// Pinar del Rio ...
-// const objProv = {
-//   "Pinar del Río": [
-//     "Centro Ciudad",
-//     "La Conchita",
-//     "Hermanos Cruz",
-//     "La Coloma",
-//     "Briones Montoto",
-//     "Calle Nueva",
-//     "La Sabana",
-//     "Las Taironas"
-//   ],
-//   "Consolación del Sur": [
-//     "Consolación Centro",
-//     "Paso Real de San Diego",
-//     "San Diego de los Baños",
-//     "Entronque de Herradura",
-//     "La Ceiba",
-//     "Las Pozas",
-//     "Los Palacios"
-//   ],
-//   "Minas de Matahambre": [
-//     "Minas de Matahambre Centro",
-//     "Santa Lucía",
-//     "Sumidero",
-//     "Galope",
-//     "Pons",
-//     "San Cayetano"
-//   ],
-//   "Viñales": [
-//     "Centro Viñales",
-//     "Los Jazmines",
-//     "El Palmarito",
-//     "Dos Hermanas",
-//     "San Vicente",
-//     "La Ermita",
-//     "El Cuajaní"
-//   ],
-//   "La Palma": [
-//     "Centro La Palma",
-//     "Cuba Libre",
-//     "Santa Bárbara",
-//     "Las Cadenas",
-//     "El Moncada",
-//     "Las Martinas"
-//   ],
-//   "Los Palacios": [
-//     "Centro Los Palacios",
-//     "San Cristóbal",
-//     "San Diego de los Baños",
-//     "Entronque de San Diego",
-//     "La Güira",
-//     "Santa María"
-//   ],
-//   "San Juan y Martínez": [
-//     "Centro San Juan y Martínez",
-//     "Guane",
-//     "El Valle de San Juan",
-//     "Las Cadenas",
-//     "La Conchita",
-//     "La Sabana"
-//   ],
-//   "San Luis": [
-//     "Centro San Luis",
-//     "Santa María",
-//     "La Coloma",
-//     "Las Taironas",
-//     "El Moncada",
-//     "La Sabana"
-//   ],
-//   "Guane": [
-//     "Centro Guane",
-//     "Sábalo",
-//     "Mantua",
-//     "Santa Lucía",
-//     "Sumidero",
-//     "Galope"
-//   ],
-//   "Sandino": [
-//     "Centro Sandino",
-//     "Cayo Ramona",
-//     "Cayo Gloria",
-//     "Cayo Jutías",
-//     "Cayo Levisa",
-//     "Cayo Buenavista"
-//   ],
-//   "Mantua": [
-//     "Centro Mantua",
-//     "Santa Lucía",
-//     "Sumidero",
-//     "Galope",
-//     "Pons",
-//     "San Cayetano"
-//   ]
-// };
 
-// let count_total = 0
+const localidadesCienfuegos = {
+  "Cienfuegos": [
+    "Centro Ciudad",
+    "Punta Gorda",
+    "La Juanita",
+    "Reina",
+    "Buena Vista",
+    "San Lázaro"
+  ],
+  "Aguada de Pasajeros": [
+    "Centro Aguada",
+    "Guayacanes",
+    "La Sierrita",
+    "Las Moscas",
+    "El Francés",
+    "San Fernando"
+  ],
+  "Rodas": [
+    "Centro Rodas",
+    "Aguada de Pasajeros",
+    "Guayacanes",
+    "La Sierrita",
+    "Las Moscas",
+    "El Francés"
+  ],
+  "Palmira": [
+    "Centro Palmira",
+    "Cruces",
+    "Lajas",
+    "Aguada de Pasajeros",
+    "Rodas",
+    "Guayacanes"
+  ],
+  "Lajas": [
+    "Centro Lajas",
+    "Cruces",
+    "Palmira",
+    "Aguada de Pasajeros",
+    "Rodas",
+    "Guayacanes"
+  ],
+  "Cruces": [
+    "Centro Cruces",
+    "Palmira",
+    "Lajas",
+    "Aguada de Pasajeros",
+    "Rodas",
+    "Guayacanes"
+  ],
+  "Cumanayagua": [
+    "Centro Cumanayagua",
+    "Aguada de Pasajeros",
+    "Rodas",
+    "Guayacanes",
+    "La Sierrita",
+    "Las Moscas"
+  ],
+  "Abreus": [
+    "Centro Abreus",
+    "Rodas",
+    "Guayacanes",
+    "La Sierrita",
+    "Las Moscas",
+    "El Francés"
+  ],
+  "Santa Isabel de las Lajas": [
+    "Centro Santa Isabel",
+    "Abreus",
+    "Rodas",
+    "Guayacanes",
+    "La Sierrita",
+    "Las Moscas"
+  ]
+};
 
-// Object.entries(objProv).forEach(([municipio, localidades]) => {
+let count_total = 0
 
-//   console.log(`\nmunicipio -> ${municipio}\n`)
-//   let count_loc = 0
+Object.entries(localidadesCienfuegos).forEach(([municipio, localidades]) => {
 
-//   localidades.forEach(loc => {
-//     db.addTown(loc, municipio)
-//     console.log(`  --> (${loc}) agregada`)
-//     count_loc++
-//   })
-//   console.log(`---------------------------------------------- ${count_loc} - agregados -------`);
+  console.log(`\nmunicipio -> ${municipio}\n`)
+  let count_loc = 0
 
-//   count_total += count_loc
-// })
-// console.log(`\n************** ${count_total} - Total de Localidades Agreagadas **************`);
+  localidades.forEach(loc => {
+    db.addTown(loc, municipio)
+    console.log(`  --> (${loc}) agregada`)
+    count_loc++
+  })
+  console.log(`---------------------------------------------- ${count_loc} - agregados -------`);
+
+  count_total += count_loc
+})
+console.log(`\n************** ${count_total} - Total de Localidades Agreagadas **************`);
 
 
 
