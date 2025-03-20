6068fd01ac598f22042e1a21596bdc56d73d639d
alexdev <alexdev.workenv@gmail.com>
2025-03-20 18:33:38 -0400

    add towns of Camaguey, Las Tunas, Granma, Santiago de Cuba, Guantanamo

--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,20 @@
 import db from './db-manage';
 
 
-const localidadesCiegoDeAvila = {
-  "Ciego de Ávila": [
-    "Centro Ciudad",
-    "Venezuela",
-    "Baraguá",
-    "Primero de Enero",
-    "Chambas",
-    "Florencia"
-  ],
-  "Morón": [
-    "Centro Morón",
-    "Ciro Redondo",
-    "Chambas",
-    "Florencia",
-    "Majagua",
-    "Venezuela"
-  ],
-  "Chambas": [
-    "Centro Chambas",
-    "Florencia",
-    "Majagua",
-    "Venezuela",
-    "Morón",
-    "Ciro Redondo"
-  ],
-  "Majagua": [
-    "Centro Majagua",
-    "Venezuela",
-    "Morón",
-    "Ciro Redondo",
-    "Chambas",
-    "Florencia"
-  ],
-  "Florencia": [
-    "Centro Florencia",
-    "Chambas",
-    "Majagua",
-    "Venezuela",
-    "Morón",
-    "Ciro Redondo"
-  ],
-  "Venezuela": [
-    "Centro Venezuela",
-    "Baraguá",
-    "Primero de Enero",
-    "Ciego de Ávila",
-    "Morón",
-    "Chambas"
-  ],
-  "Baraguá": [
-    "Centro Baraguá",
-    "Primero de Enero",
-    "Ciego de Ávila",
-    "Venezuela",
-    "Morón",
-    "Chambas"
-  ],
-  "Primero de Enero": [
-    "Centro Primero de Enero",
-    "Ciego de Ávila",
-    "Venezuela",
-    "Baraguá",
-    "Morón",
-    "Chambas"
-  ],
-  "Ciro Redondo": [
-    "Centro Ciro Redondo",
-    "Morón",
-    "Chambas",
-    "Florencia",
-    "Majagua",
-    "Venezuela"
+const localidadesIslaDeJuventud = {
+  "Isla de la Juventud": [
+    "Nueva Gerona",
+    "Santa Fe",
+    "La Demajagua",
+    "Cayo Largo del Sur",
+    "Punta del Este",
+    "Columbia"
   ]
 };
 
 let count_total = 0
 
-Object.entries(localidadesCiegoDeAvila).forEach(([municipio, localidades]) => {
+Object.entries(localidadesIslaDeJuventud).forEach(([municipio, localidades]) => {
 
   console.log(`\nmunicipio -> ${municipio}\n`)
   let count_loc = 0
