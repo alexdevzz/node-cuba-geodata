0635a1894b9f2d49376ecf72a0c8a33f5be7b452
alexdev <alexdev.workenv@gmail.com>
2025-03-20 18:49:17 -0400

    cleaning index.ts and finalizing the database population

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,6 @@
-import db from './db-manage';
+// import db from './db-manage';
 
 
-const localidadesIslaDeJuventud = {
-  "Isla de la Juventud": [
-    "Nueva Gerona",
-    "Santa Fe",
-    "La Demajagua",
-    "Cayo Largo del Sur",
-    "Punta del Este",
-    "Columbia"
-  ]
-};
-
-let count_total = 0
-
-Object.entries(localidadesIslaDeJuventud).forEach(([municipio, localidades]) => {
-
-  console.log(`\nmunicipio -> ${municipio}\n`)
-  let count_loc = 0
-
-  localidades.forEach(loc => {
-    db.addTown(loc, municipio)
-    console.log(`  --> (${loc}) agregada`)
-    count_loc++
-  })
-  console.log(`---------------------------------------------- ${count_loc} - agregados -------`);
-
-  count_total += count_loc
-})
-console.log(`\n************** ${count_total} - Total de Localidades Agreagadas **************`);
 
 
 
