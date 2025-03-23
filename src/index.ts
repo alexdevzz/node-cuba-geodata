3916e751213f5809dcd0650d6aa1b06ef4e00105
alexdev <alexdev.workenv@gmail.com>
2025-03-23 14:48:43 -0400

    prepare to publish

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
-// import { getCities } from './services/cityService';
-// import { getProvinces } from './services/provinceService';
-import { getTowns } from './services/townService';
+import { getCities, addCity } from './services/cityService'
+import { getProvinces, addProvince } from './services/provinceService'
+import { getTowns, addTown } from './services/townService'
 
-// const aux = getProvinces('', 2)
+export {
+  getCities, addCity,
+  getProvinces, addProvince,
+  getTowns, addTown
+}
 
-// console.log(JSON.stringify(aux, null, 2));
-// console.log(aux);
-
-
-// const aux1 = getCities('', 1)
-
-// console.log(JSON.stringify(aux1, null, 2));
-// console.log(aux1);
-
-
-const aux2 = getTowns('san')
-
-// console.log(JSON.stringify(aux2, null, 2));
-console.log(aux2);
+export default {
+  getCities,
+  getProvinces,
+  getTowns
+}
 
 
