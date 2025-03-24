14d95b6ccda99d1d38bfcccd3ebc2035b56d31d2
alexdev <alexdev.workenv@gmail.com>
2025-03-24 16:28:17 -0400

    change FILE_DATABASE_PATH to use path.join and update package.json version

--- src/settings/manage.ts
+++ src/settings/manage.ts
@@ -1,10 +1,15 @@
 import Database from 'better-sqlite3'
+import path from 'path';
 
-const FILE_DATABASE_PATH = "database/db.sqlite3"
+const FILE_DATABASE_PATH = path.join(__dirname, '..', '..', 'database', 'db.sqlite3')
 
 export const dbConnect = () => {
   return new Database(FILE_DATABASE_PATH)
 }
+
+
+
+
 
 
 
